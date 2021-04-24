fe9950b1686be2a7c4e1d923f1d919aef790e572
Артем Дмитриев <artem2733@yandex.ru>
2021-04-24 20:00:08 +0300

    Add send_put method

--- features/support/utils/rest_utils.rb
+++ features/support/utils/rest_utils.rb
@@ -4,7 +4,7 @@ end
 
 def send_get(path, headers = {})
   url = path
-  RestClient::Request.execute(method: :get, url: url, headers: headers, verify_ssl: false) do |response, _request, _result|
+  RestClient::Request.execute(method: :get, url: url, headers: headers) do |response, _request, _result|
     @response = response
     case response.code
     when 301, 302, 307
@@ -16,34 +16,27 @@ def send_get(path, headers = {})
 end
 
 def send_post(url, payload, headers = {})
-  RestClient::Request.execute(method: :post, url: url, headers: headers, payload: payload, verify_ssl: false) do |response, _code|
+  RestClient::Request.execute(method: :post, url: url, headers: headers, payload: payload) do |response, _code|
+    @response = response
+  end
+  @response
+end
+
+def send_put(url, payload, headers = {})
+  RestClient::Request.execute(method: :put, url: url, headers: headers, payload: payload) do |response, _code|
     @response = response
   end
   @response
 end
 
 def send_delete(path, headers = {})
-  RestClient::Request.execute(method: :delete, url: path, headers: headers, verify_ssl: false) do |response, _request, _result|
+  RestClient::Request.execute(method: :delete, url: path, headers: headers) do |response, _request, _result|
     @response = response
-    case response.cod
+    case response.code
     when 301, 302, 307
       @response = response.follow_redirection
     else
       @response = response
     end
   end
-end
-
-def log_request(url, payload, headers)
-  p 'ПАРАМЕТРЫ ЗАПРОСА:'
-  p url
-  p payload
-  p headers
-end
-
-def log_response(response)
-  p 'ПАРАМЕТРЫ ОТВЕТА:'
-  p "response_code = #{response.code} "
-  pp "response_head = #{response.headers} "
-  pp "response_body = #{JSON.parse(response.body)} "
 end
