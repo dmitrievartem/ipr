5e470974655919349ec5fdb18bff8682dc93d1a7
Артем Дмитриев <artem2733@yandex.ru>
2021-04-25 19:51:21 +0300

    Update sh file

--- run_cucumber.sh
+++ run_cucumber.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sh printenv
+sh 'printenv'
 
 echo 'salam'
 
-sh bundle exec cucumber -t @ui --publish-quiet -f pretty -f json --out report_files/report.json
+sh 'bundle exec cucumber -t @ui --publish-quiet -f pretty -f json --out report_files/report.json'
 
 #printenvgit
