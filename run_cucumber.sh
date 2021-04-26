c51a97cb06be687177a62418254fd2e3979c8c2a
Артем Дмитриев <artem2733@yandex.ru>
2021-04-26 15:47:39 +0300

    update run_cucumber.sh

--- run_cucumber.sh
+++ run_cucumber.sh
@@ -7,7 +7,14 @@ ruby -v
 pwd
 bundler install
 
+if [ "${TAGS}" ]; then
+echo "MY_PARAMETER = ${TAGS}"
+else
+echo "MY_PARAMETER = ${TAGS}"
+fi
+
 echo "${TAGS}"
+echo"!!!!!!!!!!!!!!!!!!!"
 bundle exec cucumber --publish-quiet -f pretty -f json --out report_files/report.json --tags '${TAGS}'
 
 #printenv
