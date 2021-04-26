6744c889f478f8c2c70ad1852d4a6c20ff9b1535
Артем Дмитриев <artem2733@yandex.ru>
2021-04-26 16:51:45 +0300

    update run_cucumber.sh

--- run_cucumber.sh
+++ run_cucumber.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 echo 'something'
-echo ["${TAGS}"]
+
 if [ "${TAGS}" = "true" ]; then
 echo "MY_PARAMETER = ${TAGS}"
 else
 echo "MY_PARAMETER = ${TAGS}"
 fi
-
-
 
 export LANG=C.UTF-8 && locale
 
@@ -16,8 +14,6 @@ bundler install
 
 printenv
 
-echo "${TAGS}"
-echo "!!!!!!!!!!!!!!!!!!!"
 bundle exec cucumber --publish-quiet -f pretty -f json --out report_files/report.json --tags '${TAGS}'
 
 #printenv
