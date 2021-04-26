563f02c56661e9c76187bffc25ab910001cef1f5
Артем Дмитриев <artem2733@yandex.ru>
2021-04-26 17:01:26 +0300

    update run_cucumber.sh

--- run_cucumber.sh
+++ run_cucumber.sh
@@ -18,6 +18,6 @@ export LANG=C.UTF-8 && locale
 
 bundler install
 
-bundle exec cucumber --publish-quiet -f pretty -f json --out report_files/report.json --tags ${TAGS}
+bundle exec cucumber --publish-quiet -f pretty -f json --out report_files/report.json --tags @updatePet
 
 #printenv
