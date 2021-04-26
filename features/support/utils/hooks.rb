ac7a5139fd2a8e8d8964d7bde5a317b1b637bd1d
Артем Дмитриев <artem2733@yandex.ru>
2021-04-26 23:46:10 +0300

    Update GoolgeChromePortable

--- features/support/utils/hooks.rb
+++ features/support/utils/hooks.rb
@@ -1,7 +1,7 @@
 Before('@ui') do
   # Указываю пути до портативного браузера и хромдрайвера
   Selenium::WebDriver::Chrome::Service.driver_path = 'features/support/GoogleChromePortable/chromedriver.exe'
-  path_to_browser = 'features/support/GoogleChromePortable/GoogleChromePortable.exe'
+  path_to_browser = 'features/support/GoogleChromePortable/chromedriver.exe'
   options = Selenium::WebDriver::Chrome::Options.new(binary: path_to_browser)
   @browser = Selenium::WebDriver.for :chrome, options: options
   # Неявное ожидание
