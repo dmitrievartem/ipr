fea37cafaeb5ba331e36b65fc2c3cd877d7bbd1c
Артем Дмитриев <artem2733@yandex.ru>
2021-04-27 14:44:25 +0300

    Refacoring

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,14 +1,19 @@
 Before('@ui') do
   # Указываю пути до портативного браузера и хромдрайвера
-  Selenium::WebDriver::Chrome::Service.driver_path = 'features/support/GoogleChromePortable/chromedriver.exe'
+  # Selenium::WebDriver::Chrome::Service.driver_path = 'features/support/GoogleChromePortable/chromedriver.exe'
   # if ENV['ABSOLUTE_PATH']
   #   path_to_browser = 'D:\Ruby\RubyMine 2020.3.1\RubymineProjects\ipr\features\support\GoogleChromePortable\App\Chrome-bin\chrome.exe'
   # else
   #   path_to_browser = 'features/support/GoogleChromePortable/App/Chrome-bin/chrome.exe'
   # end
-  absolute_path = get_filepath('features/support/GoogleChromePortable/App/Chrome-bin/chrome.exe')
+  # absolute_path = get_filepath('features/support/GoogleChromePortable/App/Chrome-bin/chrome.exe')
+  # options = Selenium::WebDriver::Chrome::Options.new(binary: absolute_path.to_s)
+  # @browser = Selenium::WebDriver.for :chrome, options: options
+  Selenium::WebDriver::Chrome::Service.driver_path = 'chromedriver.exe'
+  absolute_path = get_filepath('features/support/utils/GoogleChromePortable/App/Chrome-bin/chrome.exe')
   options = Selenium::WebDriver::Chrome::Options.new(binary: absolute_path.to_s)
-  @browser = Selenium::WebDriver.for :chrome, options: options
+  @browser = Selenium::WebDriver.for(:chrome, options: options)
+  @browser.manage.window.maximize
   # Неявное ожидание
   @browser.manage.timeouts.implicit_wait = 5
 end
@@ -25,15 +30,6 @@ After('not @ui') do |scenario|
   if scenario.failed?
     add_rest_logs
   end
-end
-
-def get_filepath(filepath)
-  filename = File.join(File.expand_path(Dir.pwd), filepath)
-  unless File.file?(filename)
-    puts "file #{filename} not finded"
-    raise Errno::ENOENT
-  end
-  filename
 end
 
 def add_screenshot
---
+++ features/support/utils/utils.rb
@@ -0,0 +1,9 @@
+# Получаем абсолютный путь к файлу
+def get_filepath(filepath)
+  filename = File.join(File.expand_path(Dir.pwd), filepath)
+  unless File.file?(filename)
+    puts "file #{filename} not finded"
+    raise Errno::ENOENT
+  end
+  filename
+end
