06417c4e778c349e87c71d7856c9e280b1a137dc
Артем Дмитриев <artem2733@yandex.ru>
2021-04-28 11:09:56 +0300

    Refactoring

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,9 +6,3 @@ require 'cucumber'
 require 'mysql2'
 require 'cucumber'
 require 'rspec/expectations'
-
-@connection = Mysql2::Client.new(host:     "localhost",
-                                 username: "dmitrievas",
-                                 password: "art.KS2733",
-                                 database: "ipr",
-                                 port:     3306)
