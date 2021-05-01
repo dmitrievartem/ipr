19d711aa2df58606caaf7544de4d18d7ff0e5dd0
Артем Дмитриев <artem2733@yandex.ru>
2021-05-01 14:59:02 +0300

    Try fix browser path

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -3,13 +3,13 @@ Before('@ui') do |scenario|
   if ENV['ABSOLUTE_PATH'] == true
     path_to_browser = 'D:\Ruby\RubyMine 2020.3.1\RubymineProjects\ipr\features\support\utils\GoogleChromePortable\App\Chrome-bin\chrome.exe'
   else
-    path_to_browser = 'features/support/utils/GoogleChromePortable/App/Chrome-bin/chrome.exe'
-    path_to_browser = Dir.pwd + '/features/support/utils/GoogleChromePortable/App/Chrome-bin'
-    p 'BEFORE'
-    p path_to_browser
-    path_to_browser["C:"]= "/c"
-    p 'AFTER'
-    p path_to_browser
+    path_to_browser = './features/support/utils/GoogleChromePortable/App/Chrome-bin/chrome.exe'
+    # path_to_browser = Dir.pwd + '/features/support/utils/GoogleChromePortable/App/Chrome-bin/chrome.exe'
+    # p 'BEFORE'
+    # p path_to_browser
+    # path_to_browser["C:"]= "/c"
+    # p 'AFTER'
+    # p path_to_browser
   end
 
   # попробовать поменять слэши
